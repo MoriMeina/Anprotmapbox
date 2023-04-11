a6129a1a42fbad76d846ef0f9f50a78b4873bfbe
Meina9758 <bilibilimyhx@gmail.com>
2023-04-11 22:55:07 +0900

    新增忘记密码和修改密码页面，但未填充内容

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Home from './pages/home'
 import Login from './pages/login'
 import Register from './pages/register'
 import Upload from './pages/upload'
+import ChangePassword from './pages/changePassword'
+import ForgotPassword from './pages/forgotPassword'
 import {Route, Routes, Navigate, HashRouter} from 'react-router-dom'
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
                 <Route path={"/home"} element={<Home/>}/>
                 <Route path={"/register"} element={<Register/>}/>
                 <Route path={"/upload"} element={<Upload/>}/>
+                <Route path={"/changePassword"} element={<ChangePassword/>}/>
+                <Route path={"/forgotPassword"} element={<ForgotPassword/>}/>
             </Routes>
         </HashRouter>
     )
---
+++ src/pages/changePassword/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ChangePassword =()=>{
+    return(
+        <div>
+            <h1>Change Password</h1>
+        </div>
+    )
+}
+export default ChangePassword;
